64cd89b32138c9b69bafd4be92440f51c4da15fb
Ratna-Babu <ratna71babu.com>
2025-02-28 17:42:39 +0530

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import nextjs from "./tech/nextjs.png";
+import python from "./tech/python.png";
+
+import meta from "./company/meta.png";
+import edunet from "./company/edunet.png";
+import infosys from "./company/infosys.png";
+import ibm from "./company/ibm.png";
+
+import coalmine from "./coalmine.png";
+import exoplanets from "./exoplanets.png";
+import aiwebbuilder from "./aiwebbuilder.png";
+
+export {
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  ibm,
+  infosys,
+  edunet,
+  python,
+  nextjs,
+  coalmine,
+  exoplanets,
+  aiwebbuilder,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,236 @@
+import {
+  edunet,
+  infosys,
+  ibm,
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  meta,
+  threejs,
+  nextjs,
+  python,
+  coalmine,
+  aiwebbuilder,
+  exoplanets,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Cerfitication",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Full-stack Web Developer",
+    icon: web,
+  },
+  {
+    title: "Python Developer",
+    icon: mobile,
+  },
+  {
+    title: "Machine Learning & AI",
+    icon: backend,
+  },
+  {
+    title: "Database Management",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "nextjs",
+    icon: nextjs,
+  },
+];
+
+const experiences = [
+  {
+    title: "Artificial Intelligence and Machine Learning Internship",
+    company_name: "Edunet Foundation",
+    icon: edunet,
+    iconBg: "#E6DEDD",
+    date: "May 2024 - June 2024",
+    points: [
+      "Worked on real-world AI and ML projects, applying supervised and unsupervised learning techniques.",
+      "Gained hands-on experience with Python, TensorFlow, and data preprocessing for model training.",
+      "Collaborated with mentors and peers to solve complex AI-driven challenges.",
+    ],
+  },
+  {
+    title: "Artificial Intelligence Primer Certification",
+    company_name: "Infosys Springboard",
+    icon: infosys,
+    iconBg: "#E6DEDD",
+    date: "July 2024",
+    points: [
+      "Gained foundational knowledge of artificial intelligence concepts, including machine learning and neural networks.",
+      "Explored real-world applications of AI across various industries such as healthcare, finance, and automation.",
+      "Completed hands-on projects to understand AI problem-solving approaches and ethical considerations."
+    ],
+  },
+  {
+    title: "Meta Front-End Developer Professional Certificate",
+    company_name: "Coursera – Pursuing",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Dec 2024 - Present",
+    points: [
+      "Learning modern front-end development technologies, including React.js, JavaScript, and responsive web design.",
+      "Developing interactive and accessible web applications while following industry best practices.",
+      "Gaining experience in version control (Git/GitHub) and optimizing web performance.",
+    ],
+  },
+  
+  {
+    title: "Data Analyst Certification",
+    company_name: "IBM – Pursuing",
+    icon: ibm,
+    iconBg: "#E6DEDD",
+    date: "Jan 2025 - Present",
+    points: [
+      "Learning key data analysis techniques using Python, SQL, and Excel for data-driven decision-making.",
+      "Understanding statistical methods, data wrangling, and predictive analytics.",
+      "Exploring data visualization and storytelling through tools like Tableau and Matplotlib."
+    ],
+  },
+];
+
+
+const projects = [
+  {
+    name: "AI-Powered Website Builder",
+    description:
+      "Developed a website generator using Gemini AI, which creates fully functional Vite applications based on user prompts. The system dynamically generates project files and code, streamlining web development and reducing manual effort.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "gemini-ai",
+        color: "green-text-gradient",
+      },
+      {
+        name: "convex",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: aiwebbuilder,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Coal Mines Monitoring Web Application",
+    description:
+      " A web application specifically designed for Indian coal mines to quantify their carbon footprint and explore pathways to carbon neutrality. This project is developed specifically for SIH 2024 Hackathon",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "flask",
+        color: "green-text-gradient",
+      },
+      {
+        name: "material-ui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: coalmine,
+    source_code_link: "https://github.com/Ratna-Babu/Coal-Mines-Carbon-Neutrality-Web-Application",
+  },
+  {
+    name: "Exoplanets 3D Model",
+    description:
+      "Exoplanets 3D Model – A dynamic 3D visualization of exoplanets using Three.js and NASA's exoplanet archive data. The project renders realistic planetary models, allowing users to explore different exoplanets interactively.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "three.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "flask-api",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: exoplanets,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, projects };
